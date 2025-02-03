80d78a87b164a6dc0e40d64643bbd7d56b0aad65
heeeun <jungheeeun272@gmail.com>
2025-02-03 17:20:56 +0900

    useEffect 여러 개의 값 검사하기

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -5,17 +5,26 @@ import Viewer from './component/Viewer';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState(""); 
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  // onChange 이벤트 헨들러 함수 handleChangeText 만들기
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  }
   
   useEffect(() => {
-    console.log("count 업데이트: ", count);
-  }, [count]);
+    console.log("count 업데이트: ", text, count);
+  }, [count, text]);
 
   return (
   <div className='App'>
     <h1>Simple Counter</h1>
+    <section>
+    {/* 입력폼 임시로 생성하기 */}
+      <input value={text} onChange={handleChangeText} />
+    </section>
     <section>
       <Viewer count={count} /> 
     </section>
