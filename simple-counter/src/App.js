eb2e87c92d506182189bace25daa69dabcb5aff7
heeeun <jungheeeun272@gmail.com>
2025-02-03 17:36:39 +0900

    useRef를 사용해 마운트 시점 제외하고 콜백 함수 실행하도록 수정

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
 
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState(""); 
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
-  // onChange 이벤트 헨들러 함수 handleChangeText 만들기
   const handleChangeText = (e) => {
     setText(e.target.value);
   }
-  
+  // 현재 App 컴포넌트 페이지에 마운트 했는지 판단하는 변수 didMountRef를 Ref 객체로 생성한다. 초깃값으로 false 설정함.
+  // Ref 객체는 돔 요소를 참조하는 것뿐만 아니라 컴포넌트의 변수로도 자주 활용됨. 
+  const didMountRef = useRef(false);
+
   useEffect(() => {
-    console.log("count 업데이트: ", text, count);
-  }, [count, text]);
+    // 컴포넌트 마운트 시점에는 콘솔을 출력하지 않도록 조건문 추가
+    if(!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    }else{
+    console.log("컴포넌트 업데이트!");
+    }
+  });
 
   return (
   <div className='App'>
