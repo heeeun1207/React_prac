bd347fb25f95846d6e9ac5758826514a8a5856d7
heeeun <jungheeeun272@gmail.com>
2025-02-03 15:46:33 +0900

    Controller 컴포넌트 만들기

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Controller from './component/Controller';
 import Viewer from './component/Viewer';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <h1>Simple Counter</h1>
     <section>
       <Viewer />
+    </section>
+    <section>
+      <Controller />
     </section>
   </div>
   );
