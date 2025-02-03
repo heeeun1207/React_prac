90e6a18181f2544ea0c31aad68cd728e430b4ab9
heeeun <jungheeeun272@gmail.com>
2025-02-03 16:38:02 +0900

    카운트 기능 구현 state(Viewer)/ set(Controller)

--- simple-counter/src/App.js
+++ simple-counter/src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleSetCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
   <div className='App'>
     <h1>Simple Counter</h1>
     <section>
-      <Viewer />
+      {/* Viewer 컴포넌트에 state 변수 값을 props 로 전달함. */}
+      <Viewer count={count} /> 
     </section>
     <section>
-      <Controller />
+      {/* Controller 컴포넌트에 state 값을 변경하는 함수 setCount 를 props 로 전달함.  */}
+      <Controller handleSetCount={handleSetCount }/>
     </section>
   </div>
   );
