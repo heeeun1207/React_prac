94c9e006e8b5d7f300912ffce47a859e70513bf5
heeeun <jungheeeun272@gmail.com>
2025-02-06 15:06:01 +0900

    빈 입력 방지 기능 추가

--- todoapp/src/component/TodoEditor.js
+++ todoapp/src/component/TodoEditor.js
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useRef,useState } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor = ({onCreate}) => {
   const [content, setContent] = useState("");
+  const inputRef = useRef(); // 입력 폼 제어할 inputRef 변수 생성
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
   const onSubmit = () => {
+    if(!content) {
+      inputRef.current.focus();
+      return;
+    }
     onCreate(content);
   };
 
@@ -15,6 +20,7 @@ const TodoEditor = ({onCreate}) => {
     <h4>새로운 Todo 작성하기 🖌</h4>
     <div className="editor_wrapper">
       <input 
+        ref={inputRef}
         value={content}
         onChange={onChangeContent}
         placeholder="새로운 Todo..."
