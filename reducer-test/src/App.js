05d8293ec4fd95dd759fbb208edaff76601a6d13
heeeun <jungheeeun272@gmail.com>
2025-02-10 20:01:42 +0900

    입력 값(number)만큼 증감하도록 코드 수정

--- reducer-test/src/App.js
+++ reducer-test/src/App.js
@@ -1,30 +1,35 @@
-import {useReducer} from 'react';
+import {useReducer, useState} from 'react';
 
 function App() {
-  function countReducer(oldCount,action){
-    if(action === "UP") {
-        return oldCount + 1;
-    } else if(action === "DOWN") {
-        return oldCount -1
-    } else if(action === "RESET") {
+  const [number, setNumber] = useState(1);
+  function countReducer(oldCount, action){
+    if(action.type === "UP") {
+        return oldCount + action.number;
+    } else if(action.type === "DOWN") {
+        return oldCount - action.number;
+    } else if(action.type === "RESET") {
       return 0;
     }
   }
-  const [count, countdispatch] = useReducer(countReducer, 0);
+  const [count, countDispatch] = useReducer(countReducer, 0);
   function down() {
-    countdispatch("DOWN");
+    countDispatch({ type:"DOWN", number: number });
   }
   function reset() {
-    countdispatch("RESET");
+    countDispatch({ type:"RESET", number: number });
   }
   function up() {
-    countdispatch("UP");
+    countDispatch({ type:"UP", number: number });
   }
-  return (
+  function changeNumber(event){ 
+    setNumber(Number(event.target.value))
+  }
+  return ( 
     <div className="App">
       <input type="button" value="-" onClick={down} />
       <input type="button" value="0" onClick={reset} />
       <input type="button" value="+" onClick={up} />
+      <input type="text" value={number} onChange={changeNumber} />
       <span>{count}</span>
     </div>
   );
