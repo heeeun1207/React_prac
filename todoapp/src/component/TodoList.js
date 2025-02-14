2d1710e0fc558ed9fa79bcb3e4c3306443ea98c1
heeeun <jungheeeun272@gmail.com>
2025-02-14 15:51:57 +0900

    useMemo를 활용한 할 일 통계 보기

--- todoapp/src/component/TodoList.js
+++ todoapp/src/component/TodoList.js
@@ -7,16 +7,34 @@ const TodoList = ({todo, onUpdate, onDelete}) => {
   const onChangeSearch = (e) => {
     setSearch(e.target.value);
   };
-  // 할 일 아이템 필터링 기능 
+
   const getSearchResult = () => {
     return search === ""
     ? todo
     : todo.filter((it) => it.content.toLowerCase().includes(search.toLowerCase())
-  );
+    );
   }; 
+
+  const analyzeTodo = () => {
+    const totalCount = todo.length;
+    const doneCount = todo.filter((it) => it.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return{
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+  const { totalCount, doneCount, notDoneCount } = analyzeTodo();
+
   return (
     <div className="TodoList">
       <h4>Todo List ☘</h4>
+      <div>
+        <div>총개수: {totalCount}</div>
+        <div>완료된 할 일: {doneCount}</div>
+        <div>아직 완료하지 못한 할 일: {notDoneCount}</div>
+      </div>
       <input
         value={search}
         onChange={onChangeSearch} 
